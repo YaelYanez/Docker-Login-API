9916e2db378bd39b8aef291a74209bcbc576fc76
YaelYanez <yaelyanez112@gmail.com>
2020-06-11 23:23:36 +0000

    envs improvements

--- env/env.production.ts
+++ env/env.production.ts
@@ -7,7 +7,7 @@ export default {
   },
   MONGODB: {
     PORT: 27017,
-    HOST: "0.0.0.0",
+    HOST: "192.168.1.67",
     USER_NAME: "dba-Auth",
     USER_PASSWORD: "admin1234",
     DEFAULT_DATABASE: "dbAuth",
